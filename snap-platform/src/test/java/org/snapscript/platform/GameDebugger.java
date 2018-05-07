18e15c27fdaa0ffdef1ed42b765663dff00aa0b7
Niall Gallagher <gallagher_niall@yahoo.com>
2018-05-07 05:36:17 +0100

    Update version

--- snap-platform/src/test/java/org/snapscript/platform/GameDebugger.java
+++ snap-platform/src/test/java/org/snapscript/platform/GameDebugger.java
@@ -11,9 +11,11 @@ import org.snapscript.compile.Compiler;
 import org.snapscript.compile.Executable;
 import org.snapscript.compile.ResourceCompiler;
 import org.snapscript.compile.StoreContext;
+import org.snapscript.compile.verify.VerifyError;
 import org.snapscript.compile.verify.VerifyException;
 import org.snapscript.core.Context;
-import org.snapscript.compile.verify.VerifyError;
+import org.snapscript.core.scope.EmptyModel;
+import org.snapscript.core.scope.Model;
 
 public class GameDebugger {
 
@@ -29,7 +31,35 @@ public class GameDebugger {
       Executor executor = new ThreadPool(8);
       Context context = new StoreContext(store, executor);
       Compiler compiler = new ResourceCompiler(context);
-      
+
+      compile(store, executor);
+      execute(compiler);
+   }
+   
+   private static void compile(Store store, Executor executor) {
+      try {
+         for(int i = 0; i < 1000; i++) {
+            long start = System.currentTimeMillis();
+            Context context = new StoreContext(store, executor);
+            Compiler compiler = new ResourceCompiler(context);
+            Model model = new EmptyModel();
+            Executable executable = compiler.compile("/mario/MarioGame.snap");
+            executable.execute(model, true);
+            long finish = System.currentTimeMillis();
+            System.out.println("COMPILE: " +(finish-start));
+         }
+      } catch(VerifyException e){
+         List<VerifyError> errors = e.getErrors();
+         
+         for(VerifyError error : errors) {
+            System.err.println(error);
+         }
+      } catch(Exception e){
+         e.printStackTrace();
+      }
+   }
+
+   private static void execute(Compiler compiler) {
       try {
          Executable executable = compiler.compile("/mario/MarioGame.snap");
          executable.execute();
